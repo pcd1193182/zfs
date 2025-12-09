3b19b1ab73ad336d304f5375fc8c88b28be1c825
Paul Dagnelie <paul.dagnelie@klarasystems.com>
2025-12-09 15:15:39 -0800

    debug
    Signed-off-by: Paul Dagnelie <paul.dagnelie@klarasystems.com>

--- module/zfs/vdev_anyraid.c
+++ module/zfs/vdev_anyraid.c
@@ -2139,6 +2139,7 @@ rebal_try_move_one(vdev_anyraid_t *var, struct rebal_node *donor,
 			 * if so, we have to skip it.
 			*/
 			if (donor->arr[i] == donor->arr[j]) {
+				zfs_dbgmsg("Found %lld at %d, %d", (longlong_t)donor->arr[i], i, j);
 				found = B_TRUE;
 				break;
 			}
