72bbfa63c233da4083a9a43bb3336ea6620f7e94
Paul Dagnelie <paul.dagnelie@klarasystems.com>
2026-02-10 17:09:23 -0800

    use proper end value
    Signed-off-by: Paul Dagnelie <paul.dagnelie@klarasystems.com>

--- module/zfs/vdev_anyraid.c
+++ module/zfs/vdev_anyraid.c
@@ -2387,7 +2387,7 @@ tasklist_read(vdev_t *vd)
 	if (va->vd_contracting_leaf != -1) {
 		uint64_t start = MIN(vd->vdev_ms_count,
 		    updated_asize >> vd->vdev_ms_shift);
-		uint64_t end = updated_asize >> vd->vdev_ms_shift;
+		uint64_t end = vd->vdev_asize >> vd->vdev_ms_shift;
 		zfs_dbgmsg("disabling end of disk: %llu %llu", (u_longlong_t)start, (u_longlong_t)end);
 		for (uint64_t m = start; m < end; m++) {
 			metaslab_t *ms = vd->vdev_ms[m];
