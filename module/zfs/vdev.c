2f3186b7645936f837e9d71eb432be16fb2a34d3
Paul Dagnelie <paul.dagnelie@klarasystems.com>
2026-04-07 10:55:40 -0700

    Fix failfast default and usage
    Signed-off-by: Paul Dagnelie <paul.dagnelie@klarasystems.com>

--- module/zfs/vdev.c
+++ module/zfs/vdev.c
@@ -1048,6 +1048,9 @@ vdev_alloc(spa_t *spa, vdev_t **vdp, nvlist_t *nv, vdev_t *parent, uint_t id,
 		}
 	}
 
+	if (top_level)
+		vd->vdev_failfast =
+		    vdev_prop_default_numeric(VDEV_PROP_FAILFAST);
 	/*
 	 * Add ourselves to the parent's list of children.
 	 */
